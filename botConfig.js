4d8941526f56b9a187f1f334e6b08a38393014a4
Joshue018 <101685338+Joshue018@users.noreply.github.com>
2022-03-16 11:04:32 +0800

    Update botConfig.js

--- botConfig.js
+++ botConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
-    token: "YOUR_TOKEN",
-    prefix: "PREFIX",
+    token: "OTUzMTUyNjM0NTY5NzU2NzUy.YjAakA.q69MoQGeigT9d42mPtU1HkLHiOg",
+    prefix: "pls",
     admins: [
-        "PEOPLE WHO CAN USE ADD MONEY (IDS)"
+        "917687511281729556"
 ],
     debug: true,
     countChannel: "countChannelID"
