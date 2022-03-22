39f72b7753504a80092522681a35b64781045bd3
Joshue018 <101685338+Joshue018@users.noreply.github.com>
2022-03-22 12:59:19 +0800

    Update botConfig.js

--- botConfig.js
+++ botConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: "OTU1NjkwNjYzMjQzMDM0NjQ0.YjlWSQ.UJI5WV6HZxGI7jbN6RnTk1OeUWY",
+    token: "OTU1NjkwNjYzMjQzMDM0NjQ0.jlWSQ.UJI5WV6HZxGI7jbN6RnTk1OeUWY",
     prefix: "pls",
     admins: [
         "917687511281729556"
